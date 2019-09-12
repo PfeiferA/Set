24ac85ea60fd5c72aef966cac4324b4859b6d665
anna Pfeifer <anna.pfeifer78@gmail.com>
2019-09-12 17:41:06 +0200

    Projektdateien hinzufügen.

---
+++ Set/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var easySet1 = new EasySet<int>(new int[] { 1, 2, 3, 4, 5 });
+            var easySet2 = new EasySet<int>(new int[] { 4,5,6,7,8 });
+            var easySet3 = new EasySet<int>(new int[] { 3,4,5 });
+
+            Console.Write("Union: ");
+            foreach (var item in easySet1.Union(easySet2))
+            {
+                Console.Write( item+" ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Intersection: ");
+            foreach (var item in easySet1.Intersection(easySet2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference A \\ B: ");
+            foreach (var item in easySet1.Difference(easySet2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference B \\ A: ");
+            foreach (var item in easySet2.Difference(easySet1))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("A Subset C:  ");
+            Console.Write(easySet1.Subset(easySet3));
+            Console.WriteLine();
+
+            Console.Write("C Subset A:  ");
+            Console.Write(easySet3.Subset(easySet1));
+            Console.WriteLine();
+
+            Console.Write("C Subset B:  ");
+            Console.Write(easySet1.Subset(easySet3));
+            Console.WriteLine();
+
+            Console.Write("Symmetric Difference:  ");
+            foreach (var item in easySet1.SymmetricDifference(easySet2))
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
